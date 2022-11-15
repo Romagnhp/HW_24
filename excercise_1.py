99e8a4acd09bf28b5a378ed2ca3e720f36278aed
Romagnhp <romagnhp@gmail.com>
2022-11-15 10:34:27 +0200

    edit

--- excercise_1.py
+++ excercise_1.py
@@ -1,26 +1,36 @@
+# Создание класса Car
 class Car:
-    def __init__(self, model, year, manufacturing, engineCapacity, color) -> None:
+    def __init__(self, model, year, manufacturing, engineCapacity, price) -> None:
         self.model = model
         self.year = year
         self.manufacturing = manufacturing
         self.engineCapacity = engineCapacity
-        self.color = color
-        self.__price = None
+#закрытое свойство color .        
+        self.__color = "white"
+        self.price = price
 
-    def setPrice(self,value):
-        self._price = value
+# метод класса, для изменения свойства color
+    def setColor(self,value):
+        self.__color = value
 
-    def getPrice(self):
-        return self._price
-
-# метод для вывода данных
+# метод класса для обращения к закрытому значению color    
+    def getColor(self):
+        return self.__color
+  
+# перегрузка магического метода  __str__, базового класса Object
     def __str__(self) -> str:
-        return f"Model - {self.model} \nYear - {self.year} \nManufacturing - {self.manufacturing} \nCapaciti of engine - {self.engineCapacity} \nColor - {self.color} \n"  
+        return f"\nModel - {self.model} \nYear - {self.year} \nManufacturing - {self.manufacturing} \nCapaciti of engine - {self.engineCapacity} л \nColor - {self.__color}\nPrice - {self.price} EURO \n"  
 
-car1 = Car("Rapid", 2019, "Skoda", 1.6, "white")
+# создание объекта класса Car
+car1 = Car("Rapid", 2019, "Skoda", 1.6, 500)
 
 print(car1)
-n =input("\nВведите цену авто = ")
-car1.setPrice(n)
-print(f"Price =  {car1.getPrice()}")
+
+n = input("Введите желаемый цвет авто = ")
+car1.setColor(n)
+
+# Если цвет авто не белый добавить к цене авто 10%
+if not car1.getColor() == "white":
+    car1.price = car1.price + (car1.price * 0.1)
+    
 print(car1)
